aec9571d431b0b0ce58a1e2a4b2a66f6701de669
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-30 12:19:59 +0000

    Update some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,26 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    extends: [js.configs.recommended],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-duplicate-imports": ["error", { includeExports: true }],
+      "no-template-curly-in-string": "error",
+      "no-unmodified-loop-condition": "error",
+      "no-unreachable-loop": "error",
+      "no-use-before-define": [
+        "error",
+        {
+          functions: false,
+          classes: false,
+          variables: true,
+          allowNamedExports: false,
+        },
+      ],
+      "no-unused-vars": "error",
+      "no-self-assign": "error",
+    },
   },
 ]);
